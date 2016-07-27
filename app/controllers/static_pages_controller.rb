993900ff632950def51a6223cd7d646fb433a694
tuanvc <tuanvcw@gmail.com>
2016-07-27 19:05:52 +0700

    Done Static Pages and test module

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,15 @@ class StaticPagesController < ApplicationController
   def home
   end
 
+  def pictures
+  end
+
+  def videos
+  end
+
+  def about
+  end
+
   def help
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,44 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = " | TuanVC"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home#{@base_title}"
+    assert_select "h2", "Chào các bạn, mình là Tún"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home#{@base_title}"
+  end
+
+  test "should get pictures" do
+    get static_pages_pictures_url
+    assert_response :success
+    assert_select "title", "Pictures#{@base_title}"
+  end
+  
+  test "should get videos" do
+    get static_pages_videos_url
+    assert_response :success
+    assert_select "title", "Videos#{@base_title}"
+  end
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About#{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help#{@base_title}"
   end
-
 end
