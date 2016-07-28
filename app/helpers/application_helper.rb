28038735af1440c18cae48a0137f8976a70770e1
tuanvc <tuanvcw@gmail.com>
2016-07-28 18:20:18 +0700

    full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return full title
+  def full_title(page_title = '')
+    base_title = "TuanVC"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
