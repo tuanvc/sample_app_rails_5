5aa5f51922a4b03638cb2fc9495651be41451ad8
tuanvc <tuanvcw@gmail.com>
2016-09-03 08:11:06 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/map', to: 'application#map'
 
   get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
