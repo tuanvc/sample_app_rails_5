79260bff52f595a1c3b560598bfcd898daf4fae2
tuanvc <tuanvcw@gmail.com>
2016-09-10 14:20:56 +0700

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Chim nho", email: "chimnho@tuanvc.com", password: "foobar", password_confirmation: "foobar", admin: true)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@tuanvc.com"
+  password = "password"
+  User.create!(name: name, email: email, password: password, password_confirmation: password)
+end
